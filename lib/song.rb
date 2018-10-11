f223817825aa0780b574ea9d0b82882ccfb822f7
Cara Morelli <caramorelli7@gmail.com>
2018-10-11 13:36:22 +0000

    automatically backed up by learn

--- lib/song.rb
+++ lib/song.rb
@@ -11,7 +11,7 @@ class Song
   end 
   
   def artist_name=(name)
-    artist = Artist.find_by
+    artist = Artist.find_or_create_by_name(name)
     # @artist = artist
     artist.add_song(self)
   end 
